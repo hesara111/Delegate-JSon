5444b0cd87dc3addc36385387a6ae941992a6b88
hesara111 <dimanyel@gmail.com>
2018-12-17 04:49:19 +0200

    Закинул проект на гитхаб

---
+++ Calculator.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Data;
+
+
+namespace JSon
+{
+
+    delegate string Log();
+    class Calculator
+    {
+        public string operations { get; set; }
+        public string numbers { get; set; }
+        Log b;
+        private char[] cr = new char[200];
+        
+        public void Do()
+        {
+            for(int i=0;i<operations.Length;i++)
+            {
+
+                switch (operations[i])
+                {
+                    case '+':
+                        {
+                            b += Add;
+                           Console.WriteLine(b());
+                            break;
+                        }
+                    case '-':
+                        {
+                            b += Sub;
+                            Console.WriteLine(b());
+                            break;
+                        }
+                    case '/':
+                        {
+                            b += Div;
+                            Console.WriteLine(b());
+                            break;
+                        }
+                    case '*':
+                        {
+                            b += Multi;
+                            Console.WriteLine(b());
+                            break;
+                        }
+                    case ',':
+                        {
+                            break;
+                        }
+                }
+            }
+        }
+
+        public string Add()
+        {
+
+            string str = "";
+            for(int i=0;i<numbers.Length;i++)
+            {
+                cr[i] = numbers[i];
+                if(cr[i] ==',')
+                {
+                    cr[i]='+';
+                }
+                str += cr[i];
+            }
+            var result = (int)new DataTable().Compute(str,null) ;
+            return str+"= "+result;
+        }
+
+        public string Sub()
+        {
+            string str = "";
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                cr[i] = numbers[i];
+                if (cr[i] == ',')
+                {
+                    cr[i] = '-';
+                }
+                str += cr[i];
+            }
+            var result = (int)new DataTable().Compute(str, null);
+            return str + "= " + result;
+        }
+
+        public string Div()
+        {
+            string str = "";
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                cr[i] = numbers[i];
+                if (cr[i] == ',')
+                {
+                    cr[i] = '/';
+                }
+                str += cr[i];
+            }
+            var result = (double)new DataTable().Compute(str, null);
+            return str + "= " + result;
+        }
+
+    
+        public string Multi()
+        {
+            string str = "";
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                cr[i] = numbers[i];
+                if (cr[i] == ',')
+                {
+                    cr[i] = '*';
+                }
+                str += cr[i];
+            }
+            int result = (int)new DataTable().Compute(str, null);
+            return str + "= " + result;
+        }   
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace JSon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calc = JsonConvert.DeserializeObject<Calculator>(File.ReadAllText("File.json"));
+            calc.Do();
+        }
+    }
+}
